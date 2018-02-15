fddcad321be5672871be36f7e365a18d6da011a4
iParaClient <iParaClient@IPARACLIENT-PC>
2018-02-15 14:30:43 +0300

    [UPDATE]: teminal log message was altered

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app
     })
     .listen(port, _ => {
         console.log("\x1b[36m%s\x1b[0m", `iPara projesi şu url de çalışıyor : http://localhost:${port}`);
-        console.log("\x1b[32m%s\x1b[0m", "- Server'i durdurmak icin terminali kapatin -")
+        console.log("\x1b[32m%s\x1b[0m", "- Server'i durdurmak icin terminali kapatin ya da terminalde CTRL-C tuşlayın -")
     });
